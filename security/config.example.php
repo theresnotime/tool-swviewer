07da30a6328db60997af8b616a411ec640528acc
Sam <sam@theresnotime.co.uk>
2022-03-30 16:52:40 +0100

    Start to tidy the file path Add example config files Add composer.json

---
+++ security/config.example.php
@@ -0,0 +1,11 @@
+<?php
+$url = 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth';
+$consumerKey = '';
+$consumerSecret = '';
+
+return Array(
+    'url' => $url,
+    'consumer_key' => $consumerKey,
+    'consumer_secret' => $consumerSecret
+);
+?>
